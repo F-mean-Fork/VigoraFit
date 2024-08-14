d254fe6bcd80ccfae05f38ae5b141374b1bcbf58
Ivan Lukiev <ivan.lukiev@mail.ru>
2024-08-14 11:55:46 +0300

    updated ulrs for photos, js file and burger behavior

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
-(function() {
-    const header = document.querySelector('.header')
-    window.onscroll = () => {
-        if (document.documentElement.scrollTop > 80) {
-            header.classList.add('header_active');
-        } else {
-            header.classList.remove('header_active');
-        }
-    };
-}());
-
-// Buger header
-
-(function () {
+(() => {
+    const header = document.querySelector('.header');
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header_nav');
     const menuCloseItem = document.querySelector('.header_nav-close');
-    // console.log(burgerItem);
+    const burgerLinks = document.querySelectorAll('.header_link');
+  
+    window.onscroll = () => {
+      header.classList.toggle('header_active', document.documentElement.scrollTop > 80);
+    };
+  
     burgerItem.addEventListener('click', () => {
-        menu.classList.add('header_nav_active');
-        // console.log(1)
+      menu.classList.add('header_nav_active');
     });
+  
     menuCloseItem.addEventListener('click', () => {
-        menu.classList.remove('header_nav_active');
-        // console.log(1)
+      menu.classList.remove('header_nav_active');
     });
-}())
+  
+    burgerLinks.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href');
+        const isAnchorLink = targetId.startsWith('#');
+  
+        if (isAnchorLink) {
+          document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
+        } else {
+          window.location.href = targetId;
+        }
+  
+        menu.classList.remove('header_nav_active');
+      });
+    });
+})();
